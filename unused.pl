96e84a86767add2057b709cad8d10d1c69461649
Michael Meeks <michael.meeks@suse.com>
2012-01-06 21:20:56 +0000

    output date too to provide more accurate progress on unused code removal

--- unused.pl
+++ unused.pl
@@ -1,23 +1,26 @@
 #!/usr/bin/perl -w
 
 use strict;
+use DateTime::Format::Strptime;
 
 # grok the easy hacks data into something fun
 
 my $fh;
 my $date = '';
 my @changes = ();
-print "Lines\n";
+my $timeparser = DateTime::Format::Strptime->new( pattern => '%a %b %d %H:%M:%S %Y' );
+
+print "Date\tLines\n";
 open ($fh, "git log --numstat unusedcode.easy|") || die "Can't git log: $!";
 while (<$fh>) {
 #    print "$_";
-    if (/^Date:\s*(.*)$/) {
+    if (/^Date:\s*(.*)\+(\d+)\s*$/) {
 	$date = $1;
     } elsif (/^(\d+)\s+(\d+)\s+/) {
 	my %change;
 	$change{plus} = $1;
 	$change{minus} = $2;
-#	$change{date} = $date;
+	$change{date} = $timeparser->parse_datetime($date);
 	push @changes, \%change;
 	my $rec = \%change;
     }
@@ -28,5 +31,5 @@ my $number = 0;
 for my $rec (reverse (@changes)) {
     $number = $number + $rec->{plus} - $rec->{minus};
 #    print "+$rec->{plus} - $rec->{minus}\n";
-    print "$number\n";
+    print $rec->{date}->date() . "\t$number\n";
 }
